d8895be489d0c0cd667ed35a173dca9f01fea371
SiaSyPeng <shiyao.peng.20@dartmouth.edu>
2017-09-29 12:31:15 -0400

    dialog showing up

--- Lab1/app/src/main/java/com/cs65/gnf/lab1/AuthDialog.java
+++ Lab1/app/src/main/java/com/cs65/gnf/lab1/AuthDialog.java
@@ -35,42 +35,42 @@ public class AuthDialog extends DialogFragment {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         // inflate dialog view
-        View v = LayoutInflater.from(getActivity()).inflate(R.layout.dialog_auth, null);
+        View dialog_view = LayoutInflater.from(getActivity()).inflate(R.layout.dialog_auth, null);
 
         AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(getActivity());
 
         // checks if password or user is valid
         // and adjust "matched/unmatched" accordingly
-        final EditText pass_re = (EditText) v.findViewById(R.id.Confirm_pass);
+        final EditText pass_re = (EditText) dialog_view.findViewById(R.id.Confirm_pass);
+//
+//        pass_re.addTextChangedListener(new TextWatcher() {
+//            @Override
+//            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+//                final EditText password = getActivity().findViewById(R.id.passwrd);
+//                final TextView pass_check = getActivity().findViewById(R.id.Match);
+//                String passwords = password.getText().toString();
+//                String pass_res = pass_re.getText().toString();
+//
+//                // match/unmatch change
+//                String match = getString(R.string.pass_match);
+//
+//                if (passwords.equals(pass_res)) {
+//
+//                    pass_check.setText(match);
+//                } else {
+//                    pass_check.setText(R.string.pass_unmatch);
+//                }
+//            }
+//
+//            @Override
+//            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+//
+//            @Override
+//            public void afterTextChanged(Editable editable) {}
+//        });
 
-        pass_re.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                final EditText password = getActivity().findViewById(R.id.passwrd);
-                final TextView pass_check = getActivity().findViewById(R.id.Match);
-                String passwords = password.getText().toString();
-                String pass_res = pass_re.getText().toString();
-
-                // match/unmatch change
-                String match = getString(R.string.pass_match);
-
-                if (passwords.equals(pass_res)) {
-
-                    pass_check.setText(match);
-                } else {
-                    pass_check.setText(R.string.pass_unmatch);
-                }
-            }
-
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
-
-            @Override
-            public void afterTextChanged(Editable editable) {}
-        });
-
-        alertDialogBuilder.setView(v);
-        alertDialogBuilder.setTitle(R.string.pass_dialog)
+        alertDialogBuilder.setView(dialog_view)
+                .setTitle(R.string.pass_dialog)
                 .setCancelable(false)
                 .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
                     @Override
@@ -78,12 +78,12 @@ public class AuthDialog extends DialogFragment {
                         Log.d("DIALOG", "positive clicked");
 
                         // collect password
-                        final Text pass_check = getActivity().findViewById(R.id.Match);
-                        String pass_res = pass_re.getText().toString();
-
-                        if (pass_check.equals(R.string.pass_match)){
-                            mListener.onDialogPositiveClick(AuthDialog.this);
-                        }
+//                        final Text pass_check = getActivity().findViewById(R.id.Match);
+//                        String pass_res = pass_re.getText().toString();
+//
+//                        if (pass_check.equals(R.string.pass_match)){
+//                            mListener.onDialogPositiveClick(AuthDialog.this);
+//                        }
                     }
 
                 })
@@ -92,33 +92,32 @@ public class AuthDialog extends DialogFragment {
                     public void onClick(DialogInterface dialog, int id)
                     {
                         Log.d("DIALOG", "negative clicked");
-                        mListener.onDialogPositiveClick(AuthDialog.this);
+//                        mListener.onDialogPositiveClick(AuthDialog.this);
                         dialog.cancel();
                     }
-                })
-                .create();
+                });
 
-        return alertDialogBuilder.show();
+        return alertDialogBuilder.create();
     }
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-
-        Activity activity;
-
-        if (context instanceof Activity){
-            activity= (Activity) context;
-            try {
-                // Instantiate the NoticeDialogListener so we can send events to the host
-                mListener = (DialogListener) activity;
-            } catch (ClassCastException e) {
-                // The activity doesn't implement the interface, throw exception
-                throw new ClassCastException(activity.toString()
-                        + " must implement NoticeDialogListener");
-            }
-            Log.d("DIALOG", "attached");
-        }
-
-    }
+//    @Override
+//    public void onAttach(Context context) {
+//        super.onAttach(context);
+//
+//        Activity activity;
+//
+//        if (context instanceof Activity){
+//            activity= (Activity) context;
+//            try {
+//                // Instantiate the NoticeDialogListener so we can send events to the host
+//                mListener = (DialogListener) activity;
+//            } catch (ClassCastException e) {
+//                // The activity doesn't implement the interface, throw exception
+//                throw new ClassCastException(activity.toString()
+//                        + " must implement NoticeDialogListener");
+//            }
+//            Log.d("DIALOG", "attached");
+//        }
+//
+//    }
 }
